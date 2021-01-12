09781c3e6a2e0242c154323eb45ed7d7b4da25f9
k.pimenov <pimenovkv@yandex.ru>
2021-01-12 22:19:49 +0300

    Добавлены заготовки шаблонов html-страниц

--- blog_app/views.py
+++ blog_app/views.py
@@ -1,7 +1,9 @@
 from blog_app import app
+from flask import render_template
 
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    strings = ['String 1', 'String 2', 'String 3']
+    return render_template('index.html', title='Home', user='User', strings=strings)
