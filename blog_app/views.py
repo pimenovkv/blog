d10baf90175567bdc2dabcdb34a83f8540ba3f88
k.pimenov <pimenovkv@yandex.ru>
2021-01-22 23:47:29 +0300

    Реализована разбивка постов на страницы по N постов (в index и user).

--- blog_app/views.py
+++ blog_app/views.py
@@ -1,5 +1,5 @@
 from blog_app import app, db
-from flask import render_template, flash, redirect, url_for
+from flask import render_template, flash, redirect, url_for, request
 from flask_login import current_user, login_user, logout_user
 from blog_app.models import Users, Posts
 from blog_app.forms import LoginForm, RegistrForm, NewPostForm
@@ -19,8 +19,42 @@ def index():
             db.session.add(post)
             db.session.commit()
             return redirect(url_for('index'))
-    posts = Posts.query.order_by(Posts.timestamp.desc()).all()
-    return render_template('index.html', title='Home', username=username, form=form, posts=posts)
+    page_idx = request.args.get('page', 1, type=int)
+    posts = Posts.query.order_by(Posts.timestamp.desc()).paginate(page_idx, app.config['POSTS_PER_PAGE'], False)
+    if posts.has_prev:
+        prev_url = url_for('index', page=posts.prev_num)
+    else:
+        prev_url = None
+    if posts.has_next:
+        next_url = url_for('index', page=posts.next_num)
+    else:
+        next_url = None
+    return render_template('index.html', title='Home', username=username, form=form, posts=posts.items,
+                           page_idx=page_idx, prev_url=prev_url, next_url=next_url)
+
+
+@app.route('/user', methods=['GET', 'POST'])
+def user():
+    # posts = Posts.query.filter_by(user_id=current_user.id).order_by(Posts.timestamp.desc()).all()
+    form = NewPostForm()
+    if form.validate_on_submit():  # обработка формы и валидация данных только при запросе POST
+        post = Posts(title=form.title.data, body=form.body.data, author=current_user)
+        db.session.add(post)
+        db.session.commit()
+        return redirect(url_for('user'))
+    page_idx = request.args.get('page', 1, type=int)
+    posts = Posts.query.filter_by(user_id=current_user.id).order_by(Posts.timestamp.desc()) \
+                .paginate(page_idx, app.config['POSTS_PER_PAGE'], False)
+    if posts.has_prev:
+        prev_url = url_for('user', page=posts.prev_num)
+    else:
+        prev_url = None
+    if posts.has_next:
+        next_url = url_for('user', page=posts.next_num)
+    else:
+        next_url = None
+    return render_template('user.html', title='UserX', username=current_user.name, form=form, posts=posts.items,
+                           page_idx=page_idx, prev_url=prev_url, next_url=next_url)
 
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -60,15 +94,3 @@ def register():
         flash('Redistration success!')
         return redirect(url_for('login'))
     return render_template('register.html', title='Registration', form=form)
-
-
-@app.route('/user', methods=['GET', 'POST'])
-def user():
-    posts = Posts.query.filter_by(user_id=current_user.id).order_by(Posts.timestamp.desc()).all()
-    form = NewPostForm()
-    if form.validate_on_submit():  # обработка формы и валидация данных только при запросе POST
-        post = Posts(title=form.title.data, body=form.body.data, author=current_user)
-        db.session.add(post)
-        db.session.commit()
-        return redirect(url_for('user'))
-    return render_template('user.html', title='UserX', username=current_user.name, form=form, posts=posts)
--- blog_app_config.py
+++ blog_app_config.py
@@ -6,5 +6,8 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
     SECRET_KEY = 'debug_secret_key'
+
     SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'blog_app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    POSTS_PER_PAGE = 10
